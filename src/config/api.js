c28c1e287c80c2773a603ea1c88c3e0cee11be4e
venkat reddy <chinnavenkatareddytadi@gmail.com>
2024-08-11 23:15:16 +0530

    Fixed host

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
-const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
+const DEPLOYED='https://13.60.243.29:80'
 const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL = DEPLOYED
 
 const api = axios.create({
   baseURL: API_BASE_URL,
